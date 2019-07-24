6f4eacf8e63b1ae66ed46b28b9645f302aa37f10
Patrick Dawkins <pjcdawkins@gmail.com>
2019-07-24 12:00:05 +0100

    Update react app link

--- web/index.php
+++ web/index.php
@@ -231,7 +231,7 @@ endif;
     </p>
 
 
-    <p>Disclaimer: this is a quick proof-of-concept for a much better tool. Very early preview of future version available <a href="https://master-7rqtwti-ja6y3m7x4bl4g.eu.platform.sh/">here</a> (subject to significant changes).</p>
+    <p>Disclaimer: this is a quick proof-of-concept for a much better tool. Very early preview of future version available <a href="https://harmonics.ligetiquartet.com">here</a> (subject to significant changes).</p>
 
     <p>Pitches are notated in scientific pitch notation (C4 = "middle C"), with deviations in cents (¢). 12-tone equal temperament is assumed for all pitches, and instruments' strings. N.B. most string players do not (and should not) tune in equal temperament.</p>
 
